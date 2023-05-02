2efcd7825cfe8ee7b1ca98e1fc43875eb2d15f67
rdx420 <padraramesh420@gmail.com>
2023-05-02 11:19:29 +0530

    Themes: Add more Brightness Slider from `Iconify` [3/3]
    Credit - https://github.com/Mahmud0808/Iconify

--- themes.mk
+++ themes.mk
@@ -429,7 +429,21 @@ PRODUCT_PACKAGES += \
 # Brightness slider styles
 PRODUCT_PACKAGES += \
     BrightnessSliderFilledOverlay \
-    BrightnessSliderThinOverlay
+    BrightnessSliderThinOverlay \
+    BrightnessSliderCyberpunkOverlay \
+    BrightnessSliderGradiantOverlay \
+    BrightnessSliderNeumorphOverlay \
+    BrightnessSliderOutlineOverlay \
+    BrightnessSliderShadedOverlay \
+    BrightnessSliderThinOutlineOverlay \
+    BrightnessSliderTranslucentOverlay \
+    BrightnessSliderLeafyOutlineOverlay \
+    BrightnessSliderMinimalThumbOverlay \
+    BrightnessSliderOldSchoolThumbOverlay \
+    BrightnessSliderRoundedClipOverlay \
+    BrightnessSliderThumbSliderOverlay \
+    BrightnessSliderLineOverlay \
+    BrightnessSliderLightyOverlay 
 
 # Themes
 PRODUCT_PACKAGES += \
